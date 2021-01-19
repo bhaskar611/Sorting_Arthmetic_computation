0587eef152f4ad8af312b6517c9cfb6de4e49d73
unknown <bhaskarnavuluri9@gmail.com>
2021-01-19 15:43:49 +0530

    Read dictionry in to array

--- uc1.sh
+++ uc1.sh
@@ -6,16 +6,23 @@
 	read -p "Enter first input :" a
 	read -p "Enter second input:" b
 	read -p "Enter third input :" c
-	result=$(($a + $b * $c));
-	echo "Result:" $result
-result1=$(($a * $b + $c));
-         echo "Result" $result1
-result2=`echo | awk '{print '$c+$a/$b' }'`
-         echo "Result2" $result2
-result3=`echo | awk '{print '$a%$b+$c' }'`
-         echo "Result3" $result3
+	result1=$(($a + $b * $c));
+	echo "Result:" $result1
+result2=$(($a * $b + $c));
+         echo "Result" $result2
+result3=`echo | awk '{print '$c+$a/$b' }'`
+         echo "Result2" $result3
+result4=`echo | awk '{print '$a%$b+$c' }'`
+         echo "Result3" $result4
 declare -A dict
-dict[result]=$result
-dict[result1]=$result2
+dict[result1]=$result1
 dict[result2]=$result2
 dict[result3]=$result3
+dict[result4]=$result4
+# Read dictionary value in to a array
+for((i=0; i<=${#dict[@]}; i++))
+do
+  array[i]=${dict[result$((i+1))]}
+done
+# print array
+echo "${array[@]}"
