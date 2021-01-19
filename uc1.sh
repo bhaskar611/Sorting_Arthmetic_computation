534092cf5b1b5b91f9aeb8fff1653f56a72faee2
bhaskar611 <77197575+bhaskar611@users.noreply.github.com>
2021-01-19 15:42:34 +0530

    Update uc1.sh

--- uc1.sh
+++ uc1.sh
@@ -6,16 +6,16 @@
 	read -p "Enter first input :" a
 	read -p "Enter second input:" b
 	read -p "Enter third input :" c
-	result=$(($a + $b * $c));
-	echo "Result:" $result
-result1=$(($a * $b + $c));
-         echo "Result" $result1
-result2=`echo | awk '{print '$c+$a/$b' }'`
+	result1=$(($a + $b * $c));
+	echo "Result1:" $result1
+result2=$(($a * $b + $c));
          echo "Result2" $result2
-result3=`echo | awk '{print '$a%$b+$c' }'`
+result3=`echo | awk '{print '$c+$a/$b' }'`
          echo "Result3" $result3
+result4=`echo | awk '{print '$a%$b+$c' }'`
+         echo "Result4" $result4
 declare -A dict
-dict[result]=$result
-dict[result1]=$result2
+dict[result1]=$result1
 dict[result2]=$result2
 dict[result3]=$result3
+dict[result4]=$result4
