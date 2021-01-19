7df661fc2b68e5019f3a661d9a11b37b1276c09d
unknown <bhaskarnavuluri9@gmail.com>
2021-01-19 10:25:35 +0530

    added Arthmetic opertions for input

--- uc1.sh
+++ uc1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 	echo "Arithmetic Compution And Sorting"
 
 	declare -A dict
@@ -6,3 +6,5 @@
 	read -p "Enter first input :" a
 	read -p "Enter second input:" b
 	read -p "Enter third input :" c
+	result=$(($a + $b * $c));
+	echo "Result:" $result
