138c2ce90ac44ba1adf9b24681b7e0976ba02e96
unknown <bhaskarnavuluri9@gmail.com>
2021-01-19 15:09:41 +0530

    added new arthmetic operation

--- uc1.sh
+++ uc1.sh
@@ -12,3 +12,5 @@ result1=$(($a * $b + $c));
          echo "Result" $result1
 result2=`echo | awk '{print '$c+$a/$b' }'`
          echo "Result2" $result2
+result3=`echo | awk '{print '$a%$b+$c' }'`
+         echo "Result3" $result3
