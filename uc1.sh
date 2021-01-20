c400e28160f766fad5066c31014ea86e65d26dd9
bhaskar611 <77197575+bhaskar611@users.noreply.github.com>
2021-01-20 20:49:38 +0530

    Update uc1.sh

--- uc1.sh
+++ uc1.sh
@@ -6,9 +6,9 @@
 	read -p "Enter first input :" a
 	read -p "Enter second input:" b
 	read -p "Enter third input :" c
-	result=$(($a + $b * $c));
-	echo "Result:" $result
-result1=$(($a * $b + $c));
-         echo "Result" $result1
-result2=`echo | awk '{print '$c+$a/$b' }'`
-         echo "Result2" $result2
+	result1=$(($a + $b * $c));
+	echo "Result:" $result1
+result2=$(($a * $b + $c));
+         echo "Result" $result2
+result3=`echo | awk '{print '$c+$a/$b' }'`
+         echo "Result2" $result3
