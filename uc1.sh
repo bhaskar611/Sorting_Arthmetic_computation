2cbca93172a3b2714c3b75998f0b53c776c0d316
bhaskar611 <77197575+bhaskar611@users.noreply.github.com>
2021-01-20 20:48:08 +0530

    Update uc1.sh

--- uc1.sh
+++ uc1.sh
@@ -6,5 +6,5 @@
 	read -p "Enter first input :" a
 	read -p "Enter second input:" b
 	read -p "Enter third input :" c
-	result=$(($a + $b * $c));
-	echo "Result:" $result
+	result1=$(($a + $b * $c));
+	echo "Result:" $result1
